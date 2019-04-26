6d154920febc7725b240157f5feb8bee3d48558c
Batooon <rozum.ant@gmail.com>
2019-04-26 18:00:42 +0300

    Initial commit

---
+++ Node.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node
+    {
+        public int index;
+        public Node max;
+        public Node min;
+        public Node(int i=0)
+        {
+            index = i;
+        }
+        public void Add(int i)
+        {
+            if (i > index)
+                max = new Node(i);
+            else
+                min = new Node(i);
+        }
+        public Node getNode(int i)
+        {
+            if (index == i)
+                return this;
+            else if (index < i)
+                return max.getNode(i);
+            else if (index > i)
+                return min.getNode(i);
+            else
+                return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node tree = new Node();
+            int N = 0;
+            for(int i = 0; i < 15; i++)
+            {
+                N = int.Parse(Console.ReadLine());
+                tree.Add(N);
+            }
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+        }
+    }
+}
